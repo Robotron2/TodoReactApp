a09be5664ac327618182c450d514d434dee35f85
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-13 13:42:51 +0100

    Book image added and Body and some classes/elements styled

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,52 +1,60 @@
+import TodoHeader from "./TodoHeader"
+import TodoInput from "./TodoInput"
+
 const Home = () => {
 	return (
-		<div className="main-container">
-			<div className="solid-color-container">
-				<div className="todo-main">
-					<div class="todos-container">
-						<div class="todo-border">
-							<div class="todos">
-								<label class="container">
-									<input type="checkbox" />
-									<span class="checkmark"></span>
-								</label>
-								<p class="todo-item">This is a todo</p>
+		<div>
+			<div className="gradient"></div>
+			<div className="main-container">
+				<div className="solid-color-container">
+					<div className="todo-main">
+						<TodoHeader />
+						<TodoInput />
+						<div class="todos-container">
+							<div class="todo-border">
+								<div class="todos">
+									<label class="container">
+										<input type="checkbox" />
+										<span class="checkmark"></span>
+									</label>
+									<p class="todo-item">This is a todo</p>
 
-								<span class="remove-todo">X</span>
+									<span class="remove-todo">X</span>
+								</div>
+							</div>
+
+							<div class="todo-border">
+								<div class="todos">
+									<label class="container">
+										<input type="checkbox" />
+										<span class="checkmark"></span>
+									</label>
+									<p class="todo-item">This is a todo</p>
+
+									<span class="remove-todo">X</span>
+								</div>
+							</div>
+							<div class="mini-nav">
+								<p>5 items left</p>
+								<div class="todo-type">
+									<ul>
+										<li class="active">All</li>
+										<li>Active</li>
+										<li>Completed</li>
+									</ul>
+								</div>
+								<p>Clear completed</p>
 							</div>
 						</div>
 
-						<div class="todo-border">
-							<div class="todos">
-								<label class="container">
-									<input type="checkbox" />
-									<span class="checkmark"></span>
-								</label>
-								<p class="todo-item">This is a todo</p>
-
-								<span class="remove-todo">X</span>
-							</div>
-						</div>
-						<div class="mini-nav">
-							<p>5 items left</p>
-							<div class="todo-type">
+						<div class="todo-input mobile">
+							<div class="mobile-todo-type">
 								<ul>
 									<li class="active">All</li>
 									<li>Active</li>
 									<li>Completed</li>
 								</ul>
 							</div>
-							<p>Clear completed</p>
-						</div>
-					</div>
-
-					<div class="todo-input mobile">
-						<div class="mobile-todo-type">
-							<ul>
-								<li class="active">All</li>
-								<li>Active</li>
-								<li>Completed</li>
-							</ul>
 						</div>
 					</div>
 				</div>
