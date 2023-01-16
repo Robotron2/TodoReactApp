46617fc2d3ec63a89c839ae2435a2037f8fbda84
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-16 09:13:02 +0100

    New stae added.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ import TodoHeader from "./TodoHeader"
 
 const Home = () => {
 	const [newTodo, setNewTodo] = useState("")
+	const [todoAvailable, setTodoAvailable] = useState(false)
 	const [allTodos, setAllTodos] = useState([])
 
 	const checkTodo = () => {
@@ -12,6 +13,7 @@ const Home = () => {
 		console.log(checkTodo)
 		if (checkTodo.length > 0) {
 			setAllTodos(checkTodo)
+			setTodoAvailable(true)
 		}
 	}
 
@@ -55,63 +57,67 @@ const Home = () => {
 
 	return (
 		<div>
-			<div className="gradient"></div>
-			<div className="main-container">
-				<div className="solid-color-container">
-					<div className="todo-main">
-						<TodoHeader />
+			{todoAvailable && (
+				<div>
+					<div className="gradient"></div>
+					<div className="main-container">
+						<div className="solid-color-container">
+							<div className="todo-main">
+								<TodoHeader />
 
-						<div className="todo-input ">
-							<input type="text" placeholder="Write a to-do" className="text-input" value={newTodo} onChange={handleChange} />
-							<label className="container" onClick={handleClick}>
-								<i class="fa-solid fa-plus"></i>
-							</label>
-						</div>
-
-						<div className="todos-container">
-							{allTodos.map((todo, index) => {
-								return (
-									<div className="todo-border" key={index + 1}>
-										<div className="todos">
-											<label className="container">
-												<input type="checkbox" />
-												<span className="checkmark"></span>
-											</label>
-											<p className="todo-item">{todo}</p>
-
-											<span className="remove-todo">
-												<i className="fa fa-trash-o" aria-hidden="true" onClick={() => handleDelete(index)} />
-											</span>
-										</div>
-									</div>
-								)
-							})}
-
-							<div className="mini-nav">
-								<p>5 items left</p>
-								<div className="todo-type">
-									<ul>
-										<li className="active">All</li>
-										<li>Active</li>
-										<li>Completed</li>
-									</ul>
+								<div className="todo-input ">
+									<input type="text" placeholder="Write a to-do" className="text-input" value={newTodo} onChange={handleChange} />
+									<label className="container" onClick={handleClick}>
+										<i class="fa-solid fa-plus"></i>
+									</label>
 								</div>
-								<p>Clear completed</p>
-							</div>
-						</div>
 
-						<div className="todo-input mobile">
-							<div className="mobile-todo-type">
-								<ul>
-									<li className="active">All</li>
-									<li>Active</li>
-									<li>Completed</li>
-								</ul>
+								<div className="todos-container">
+									{allTodos.map((todo, index) => {
+										return (
+											<div className="todo-border" key={index + 1}>
+												<div className="todos">
+													<label className="container">
+														<input type="checkbox" />
+														<span className="checkmark"></span>
+													</label>
+													<p className="todo-item">{todo}</p>
+
+													<span className="remove-todo">
+														<i className="fa fa-trash-o" aria-hidden="true" onClick={() => handleDelete(index)} />
+													</span>
+												</div>
+											</div>
+										)
+									})}
+
+									<div className="mini-nav">
+										<p>5 items left</p>
+										<div className="todo-type">
+											<ul>
+												<li className="active">All</li>
+												<li>Active</li>
+												<li>Completed</li>
+											</ul>
+										</div>
+										<p>Clear completed</p>
+									</div>
+								</div>
+
+								<div className="todo-input mobile">
+									<div className="mobile-todo-type">
+										<ul>
+											<li className="active">All</li>
+											<li>Active</li>
+											<li>Completed</li>
+										</ul>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	)
 }
