d192e0f1ef10b6ec6de4c7be4fd6f37bd8f9ba4e
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-17 18:14:48 +0100

    NewTodo Item structure changed to Javascript Object instead of Normal Strings, random number id generated

--- src/pages/All.jsx
+++ src/pages/All.jsx
@@ -1,63 +1,31 @@
 import React from "react"
-import { useEffect } from "react"
 import { useState } from "react"
 import TodoHeader from "../components/TodoHeader"
 
 const All = () => {
-	const [newTodo, setNewTodo] = useState("")
+	// const [isEmpty, setIsEmpty] = useState(true)
 	const [isEmpty, setIsEmpty] = useState(false)
-	// const [todoAvailable, setTodoAvailable] = useState(false)
-	const [allTodos, setAllTodos] = useState([])
-	// localStorage.setItem("TodoList", JSON.stringify(["Hello, Hello"]))
+	const [todoContent, setTodoContent] = useState("")
+
+	let allTodos = [1, 2, 3, 4, 5, 6]
 
 	const handleChange = (e) => {
-		setNewTodo(e.target.value)
+		setTodoContent(e.target.value)
 	}
 
 	const handleClick = () => {
-		allTodos.push(newTodo)
-		localStorage.setItem("TodoLists", JSON.stringify(allTodos)) // Save items that fulfills all conditions
-		setAllTodos(allTodos)
+		//handleClick Creates a newTodo Item
 
-		// let matchFound = allTodos.filter((todo) => {
-		// 	return todo === newTodo
-		// })
-		// if (matchFound.length > 0) {
-		// 	setNewTodo("")
-		// } else {
-		// 	allTodos.push(newTodo)
-		// setAllTodos(allTodos)
-
-		// 	console.log(setAllTodos(allTodos))
-		// }
-		setTimeout(() => {
-			setNewTodo("")
-		}, 1)
-	}
-
-	const handleDelete = (id) => {
-		console.log(id)
-	}
-
-	const renderList = () => {
-		console.log(JSON.parse(localStorage.getItem("TodoLists")))
-		if (JSON.parse(localStorage.getItem("TodoLists")) === null) {
-			setIsEmpty(true)
-		} else {
-			setAllTodos(JSON.parse(localStorage.getItem("TodoLists")))
-			setIsEmpty(false)
+		let newTodo = {
+			todoTitle: todoContent,
+			id: Math.floor(Math.random() * 1024)
 		}
+		console.log(newTodo.id)
 	}
 
-	useEffect(() => {
-		renderList()
-	}, [])
-
-	// const handleSubmit = (e) => {
-	// 	e.preventDefault()
-	// 	setAllTodos(allTodos)
-	// 	allTodos.push(newTodo) onSubmit={handleSubmit}
-	// }
+	const handleDelete = () => {
+		console.log("Deleted")
+	}
 
 	return (
 		<div>
@@ -66,9 +34,9 @@ const All = () => {
 				<div className="solid-color-container">
 					<div className="todo-main">
 						<TodoHeader />
-
+						<center>All</center>
 						<div className="todo-input ">
-							<input type="text" placeholder="Write a to-do" className="text-input" value={newTodo} onChange={handleChange} />
+							<input type="text" placeholder="Write a to-do" className="text-input" onChange={handleChange} value={todoContent} />
 							<label className="container" onClick={handleClick}>
 								<i className="fa-solid fa-plus"></i>
 							</label>
@@ -133,3 +101,284 @@ const All = () => {
 }
 
 export default All
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// const All = () => {
+// 	// const [allTodosArray, setAllTodosArray] = useState([])
+// 	let allTodosArray = JSON.parse(localStorage.getItem("TodoLists"))
+// 	console.log(allTodosArray)
+
+// 	const [newTodo, setNewTodo] = useState("")
+// 	const [isEmpty, setIsEmpty] = useState(false)
+// 	// const [todoAvailable, setTodoAvailable] = useState(false)
+// 	const [allTodos, setAllTodos] = useState([])
+// 	// localStorage.setItem("TodoList", JSON.stringify(["Hello, Hello"]))
+
+// 	const handleChange = (e) => {
+// 		setNewTodo(e.target.value)
+// 	}
+
+// 	const handleClick = () => {
+// 		allTodos.push(newTodo)
+// 		localStorage.setItem("TodoLists", JSON.stringify(allTodos)) // Save items that fulfills all conditions
+// 		setAllTodos(allTodos)
+
+// 		// let matchFound = allTodos.filter((todo) => {
+// 		// 	return todo === newTodo
+// 		// })
+// 		// if (matchFound.length > 0) {
+// 		// 	setNewTodo("")
+// 		// } else {
+// 		// 	allTodos.push(newTodo)
+// 		// setAllTodos(allTodos)
+
+// 		// 	console.log(setAllTodos(allTodos))
+// 		// }
+// 		setTimeout(() => {
+// 			setNewTodo("")
+// 		}, 1)
+// 	}
+
+// 	const handleDelete = (id) => {
+// 		console.log(id)
+// 	}
+
+// 	const renderList = () => {
+// 		console.log(JSON.parse(localStorage.getItem("TodoLists")))
+// 		if (JSON.parse(localStorage.getItem("TodoLists")) === null) {
+// 			setIsEmpty(true)
+// 		} else {
+// 			setAllTodos(JSON.parse(localStorage.getItem("TodoLists")))
+// 			setIsEmpty(false)
+// 		}
+// 	}
+
+// 	useEffect(() => {
+// 		renderList()
+// 	}, [])
+
+// 	// const handleSubmit = (e) => {
+// 	// 	e.preventDefault()
+// 	// 	setAllTodos(allTodos)
+// 	// 	allTodos.push(newTodo) onSubmit={handleSubmit}
+// 	// }
+
+// return (
+// 	<div>
+// 		<div className="gradient"></div>
+// 		<div className="main-container">
+// 			<div className="solid-color-container">
+// 				<div className="todo-main">
+// 					<TodoHeader />
+
+// 					<div className="todo-input ">
+// 						<input type="text" placeholder="Write a to-do" className="text-input" value={newTodo} onChange={handleChange} />
+// 						<label className="container" onClick={handleClick}>
+// 							<i className="fa-solid fa-plus"></i>
+// 						</label>
+// 					</div>
+
+// 					<div className="todos-container">
+// 						{isEmpty && (
+// 							<div className="todo-empty">
+// 								<p>Empty Todo...</p>
+// 							</div>
+// 						)}
+// 						{!isEmpty &&
+// 							allTodos.map((todo, index) => {
+// 								return (
+// 									<div className="todo-border" key={index + 1}>
+// 										<div className="todos">
+// 											<label className="container">
+// 												<input type="checkbox" />
+// 												<span className="checkmark"></span>
+// 											</label>
+// 											<p className="todo-item">{todo}</p>
+
+// 											<span className="remove-todo">
+// 												<i className="fa fa-trash-o" aria-hidden="true" onClick={() => handleDelete(index)} />
+// 											</span>
+// 										</div>
+// 									</div>
+// 								)
+// 							})}
+
+// 						{!isEmpty && (
+// 							<div className="mini-nav">
+// 								<p>5 items left</p>
+// 								<div className="todo-type">
+// 									<ul>
+// 										<li className="active">All</li>
+// 										<li>Active</li>
+// 										<li>Completed</li>
+// 									</ul>
+// 								</div>
+// 								<p>Clear completed</p>
+// 							</div>
+// 						)}
+// 					</div>
+
+// 					{!isEmpty && (
+// 						<div className="todo-input mobile">
+// 							<div className="mobile-todo-type">
+// 								<ul>
+// 									<li className="active">All</li>
+// 									<li>Active</li>
+// 									<li>Completed</li>
+// 								</ul>
+// 							</div>
+// 						</div>
+// 					)}
+// 				</div>
+// 			</div>
+// 		</div>
+// 	</div>
+// )
+// }
+
+// export default All
--- src/pages/Completed.jsx
+++ src/pages/Completed.jsx
@@ -1,7 +1,139 @@
 import React from "react"
+import { useEffect } from "react"
+import { useState } from "react"
+import TodoHeader from "../components/TodoHeader"
 
 const Completed = () => {
-	return <div>Completed</div>
+	// const [allTodosArray, setAllTodosArray] = useState([])
+	let allTodosArray = JSON.parse(localStorage.getItem("TodoLists"))
+	console.log(allTodosArray)
+
+	const [newTodo, setNewTodo] = useState("")
+	const [isEmpty, setIsEmpty] = useState(false)
+	// const [todoAvailable, setTodoAvailable] = useState(false)
+	const [allTodos, setAllTodos] = useState([])
+	// localStorage.setItem("TodoList", JSON.stringify(["Hello, Hello"]))
+
+	const handleChange = (e) => {
+		setNewTodo(e.target.value)
+	}
+
+	const handleClick = () => {
+		allTodos.push(newTodo)
+		localStorage.setItem("TodoLists", JSON.stringify(allTodos)) // Save items that fulfills all conditions
+		setAllTodos(allTodos)
+
+		// let matchFound = allTodos.filter((todo) => {
+		// 	return todo === newTodo
+		// })
+		// if (matchFound.length > 0) {
+		// 	setNewTodo("")
+		// } else {
+		// 	allTodos.push(newTodo)
+		// setAllTodos(allTodos)
+
+		// 	console.log(setAllTodos(allTodos))
+		// }
+		setTimeout(() => {
+			setNewTodo("")
+		}, 1)
+	}
+
+	const handleDelete = (id) => {
+		console.log(id)
+	}
+
+	const renderList = () => {
+		console.log(JSON.parse(localStorage.getItem("TodoLists")))
+		if (JSON.parse(localStorage.getItem("TodoLists")) === null) {
+			setIsEmpty(true)
+		} else {
+			setAllTodos(JSON.parse(localStorage.getItem("TodoLists")))
+			setIsEmpty(false)
+		}
+	}
+
+	useEffect(() => {
+		renderList()
+	}, [])
+
+	// const handleSubmit = (e) => {
+	// 	e.preventDefault()
+	// 	setAllTodos(allTodos)
+	// 	allTodos.push(newTodo) onSubmit={handleSubmit}
+	// }
+
+	return (
+		<div>
+			<div className="gradient"></div>
+			<div className="main-container">
+				<div className="solid-color-container">
+					<div className="todo-main">
+						<TodoHeader />
+
+						<div className="todo-input ">
+							<input type="text" placeholder="Write a to-do" className="text-input" value={newTodo} onChange={handleChange} />
+							<label className="container" onClick={handleClick}>
+								<i className="fa-solid fa-plus"></i>
+							</label>
+						</div>
+
+						<div className="todos-container">
+							{isEmpty && (
+								<div className="todo-empty">
+									<p>Empty Todo...</p>
+								</div>
+							)}
+							{!isEmpty &&
+								allTodos.map((todo, index) => {
+									return (
+										<div className="todo-border" key={index + 1}>
+											<div className="todos">
+												<label className="container">
+													<input type="checkbox" />
+													<span className="checkmark"></span>
+												</label>
+												<p className="todo-item">{todo}</p>
+
+												<span className="remove-todo">
+													<i className="fa fa-trash-o" aria-hidden="true" onClick={() => handleDelete(index)} />
+												</span>
+											</div>
+										</div>
+									)
+								})}
+
+							{!isEmpty && (
+								<div className="mini-nav">
+									<p>5 items left</p>
+									<div className="todo-type">
+										<ul>
+											<li className="active">All</li>
+											<li>Active</li>
+											<li>Completed</li>
+										</ul>
+									</div>
+									<p>Clear completed</p>
+								</div>
+							)}
+						</div>
+
+						{!isEmpty && (
+							<div className="todo-input mobile">
+								<div className="mobile-todo-type">
+									<ul>
+										<li className="active">All</li>
+										<li>Active</li>
+										<li>Completed</li>
+									</ul>
+								</div>
+							</div>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Completed
