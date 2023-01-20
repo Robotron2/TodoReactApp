767ef6192b4f35302b8c8965228c56dcd62916ab
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-20 23:25:32 +0100

    Handlecheck created for active page

--- src/pages/Active.jsx
+++ src/pages/Active.jsx
@@ -7,6 +7,7 @@ import TodoHeader from "../components/TodoHeader"
 const Active = () => {
 	const [isActive, setIsActive] = useState([])
 	const [notActive, setNotActive] = useState(false)
+
 	// let activeTodos = JSON.parse(localStorage.getItem("TodoLists"))
 
 	const checkActive = () => {
@@ -31,6 +32,11 @@ const Active = () => {
 
 	const handleDelete = (id) => {
 		console.log(id)
+	}
+
+	const handleCheck = (todo) => {
+		console.log(todo)
+		// todo.isChecked = true
 	}
 
 	useEffect(() => {
@@ -58,10 +64,15 @@ const Active = () => {
 									return (
 										<div className="todo-border" key={activeTodo.id}>
 											<div className="todos">
-												<label className="container">
+												<label className="container" onClick={() => handleCheck(activeTodo)}>
 													<input type="checkbox" />
 													<span className="checkmark"></span>
 												</label>
+
+												{/* <label className="container">
+													<input type="checkbox" />
+													<span className="checkmark"></span>
+												</label> */}
 												<p className="todo-item">{activeTodo.todoTitle}</p>
 												<span className="remove-todo">
 													<i className="fa fa-trash-o" aria-hidden="true" onClick={() => handleDelete(activeTodo.id)} />
